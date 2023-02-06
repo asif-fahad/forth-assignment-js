efcaf21c311313bb42d14a5b70e232c1e5ef9dcf
asif-fahad <asif123fahad@gmail.comm>
2023-02-06 23:24:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Problem-1 : We have to make brackets to multiply and to do addition. then division and lastly subtraction
+function mindGame (positiveNumber) {
+    if(typeof positiveNumber != 'number'){
+        return "Provide valid number"
+    }
+    if(positiveNumber >= 0){
+        let positiveNumbers = ((positiveNumber*3)+10)/2-5
+    return positiveNumbers;
+    }
+}
