b798b718130da134e95b944dbb3c9777d98ef264
asif-fahad <asif123fahad@gmail.comm>
2023-02-07 12:46:25 +0600

    forth problem

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function mindGame (positiveNumber) {
     }
 }
 
-// Problem-2 : We have take remainder to find even or odd numbers.
+// Problem-2 : We have to use remainder to find even or odd numbers.
 function evenOdd(elements) {
     if(typeof elements != 'string'){
         return "Provide valid string"
@@ -18,4 +18,29 @@ function evenOdd(elements) {
     return "even";
     else 
     return "odd";
+}
+
+// Problem-3 : We have to take if and else to determine smaller number than seven.
+function isLGSeven(number){
+    if(typeof number != 'number'){
+        return "Provide valid number"
+    }
+    if (number - 7 < 7)
+    return number - 7;
+    else 
+    return number * 2;
+}
+
+// Problem-4 : 
+function findingBadData(numbers) {
+    if(typeof numbers != 'object'){
+        return "Provide valid array"
+    }
+    let badData = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] < 0) {
+            badData++;
+        }
+    }
+    return badData;
 }
