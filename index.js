c716ac4814534c148147095a76f1b616e459d056
asif-fahad <asif123fahad@gmail.comm>
2023-02-07 11:17:53 +0600

    second problem

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ function mindGame (positiveNumber) {
     if(typeof positiveNumber != 'number'){
         return "Provide valid number"
     }
-    if(positiveNumber >= 0){
+    if(positiveNumber > 0){
         let positiveNumbers = ((positiveNumber*3)+10)/2-5
     return positiveNumbers;
     }
+}
+
+// Problem-2 : We have take remainder to find even or odd numbers.
+function evenOdd(elements) {
+    if(typeof elements != 'string'){
+        return "Provide valid string"
+    }
+    if(elements.length % 2 == 0)
+    return "even";
+    else 
+    return "odd";
 }
